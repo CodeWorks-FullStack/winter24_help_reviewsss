111c13d3e7523b9747a1b95b3b452a894a605af1
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-22 09:45:59 -0700

    model stuff

---
+++ server/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace help_reviews.Models;
+
+public class Restaurant : RepoItem<int>
+{
+  public string Name { get; set; }
+  [Url, MaxLength(3000)] public string ImgUrl { get; set; }
+  [MaxLength(2000)] public string Description { get; set; }
+  public int Visits { get; set; }
+  public bool IsShutdown { get; set; }
+  public string CreatorId { get; set; }
+  public Profile Owner { get; set; }
+}
