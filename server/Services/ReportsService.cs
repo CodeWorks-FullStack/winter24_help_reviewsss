64bf2fcc04dea499347f32e88652097bff0b6527
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-23 12:15:34 -0700

    cannot leave reports for your won restaurant

--- server/Services/ReportsService.cs
+++ server/Services/ReportsService.cs
@@ -1,19 +1,20 @@
-
-
-
-
 namespace help_reviews.Services;
 
 public class ReportsService
 {
-  public ReportsService(ReportsRepository reportsRepository)
+  public ReportsService(ReportsRepository reportsRepository, RestaurantsService restaurantsService)
   {
     _reportsRepository = reportsRepository;
+    _restaurantsService = restaurantsService;
   }
   private readonly ReportsRepository _reportsRepository;
-
+  private readonly RestaurantsService _restaurantsService;
   internal Report CreateReport(Report reportData)
   {
+    Restaurant restaurant = _restaurantsService.GetRestaurantById(reportData.RestaurantId, reportData.CreatorId);
+
+    if (restaurant.CreatorId == reportData.CreatorId) throw new Exception($"YOU OWN THE {restaurant.Name}, {restaurant.Owner.Name}! YOU CANNOT LEAVE A REPORT FOR YOUR OWN RESTAURANT, SILLY GOOSE!");
+
     Report report = _reportsRepository.Create(reportData);
     return report;
   }
