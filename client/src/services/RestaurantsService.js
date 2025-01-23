12a748efacf610dd6a1c7339478c0f89ef9f926a
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-23 10:05:29 -0700

    storing restaurant in appstate

--- client/src/services/RestaurantsService.js
+++ client/src/services/RestaurantsService.js
@@ -7,6 +7,7 @@ class RestaurantsService {
   async getRestaurantById(restaurantId) {
     const response = await api.get(`api/restaurants/${restaurantId}`)
     logger.log('GOT RESTAURANT BY ID 🍽️', response.data)
+    AppState.activeRestaurant = new Restaurant(response.data)
   }
   async getRestaurants() {
     const response = await api.get('api/restaurants')
