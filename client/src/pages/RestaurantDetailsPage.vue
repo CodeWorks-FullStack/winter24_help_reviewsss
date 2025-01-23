e5cda9cbcdfc9f01b3b72f0aa538e7740d769fa4
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-23 10:08:10 -0700

    rendering restaurant

--- client/src/pages/RestaurantDetailsPage.vue
+++ client/src/pages/RestaurantDetailsPage.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import { restaurantsService } from '@/services/RestaurantsService.js';
 import { logger } from '@/utils/Logger.js';
 import Pop from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
+
+const restaurant = computed(() => AppState.activeRestaurant)
 
 const route = useRoute()
 
@@ -24,7 +27,13 @@ async function getRestaurantById() {
 
 
 <template>
-  <h1>Restaurant page!</h1>
+  <div v-if="restaurant" class="container-fluid">
+    <section class="row">
+      <div class="col-12">
+        <h1>{{ restaurant.name }}</h1>
+      </div>
+    </section>
+  </div>
 </template>
 
 
