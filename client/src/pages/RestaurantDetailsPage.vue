eccd51837008ede10588541ed3bfa8b516c76371
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-23 10:22:21 -0700

    added lil more styling

--- client/src/pages/RestaurantDetailsPage.vue
+++ client/src/pages/RestaurantDetailsPage.vue
@@ -27,40 +27,48 @@ async function getRestaurantById() {
 
 
 <template>
-  <div v-if="restaurant" class="container-fluid">
-    <section class="row">
-      <div class="col-12">
-        <h1 class="text-success"><b>{{ restaurant.name }}</b></h1>
-        <div class="restaurant-card">
-          <img :src="restaurant.imgUrl" :alt="'A picture of ' + restaurant.name" class="restaurant-img">
-          <div class="p-3">
-            <p class="mb-5">{{ restaurant.description }}</p>
-            <div class="d-md-flex justify-content-between align-items-center">
-              <div class="d-flex gap-5">
-                <div class="d-flex align-items-center">
-                  <i class="mdi mdi-account-multiple-outline fs-1 text-success"></i>
-                  <span><b>{{ restaurant.visits }}</b> recent visits</span>
+  <div class="p-3">
+    <div v-if="restaurant" class="container-fluid">
+      <section class="row">
+        <div class="col-12">
+          <div class="d-flex justify-content-between">
+            <h1 class="text-success"><b>{{ restaurant.name }}</b></h1>
+            <span class="fs-1 bg-danger text-light px-3">
+              <i class="mdi mdi-close-circle"></i>
+              CURRENTLY SHUTDOWN
+            </span>
+          </div>
+          <div class="restaurant-card">
+            <img :src="restaurant.imgUrl" :alt="'A picture of ' + restaurant.name" class="restaurant-img">
+            <div class="p-3">
+              <p class="mb-5">{{ restaurant.description }}</p>
+              <div class="d-md-flex justify-content-between align-items-center">
+                <div class="d-flex gap-5">
+                  <div class="d-flex align-items-center">
+                    <i class="mdi mdi-account-multiple-outline fs-1 text-success"></i>
+                    <span><b>{{ restaurant.visits }}</b> recent visits</span>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <i class="mdi mdi-file-document fs-1 text-success"></i>
+                    <span><b>0</b> reports</span>
+                  </div>
                 </div>
-                <div class="d-flex align-items-center">
-                  <i class="mdi mdi-file-document fs-1 text-success"></i>
-                  <span><b>0</b> reports</span>
+                <div class="d-flex gap-5">
+                  <button class="btn btn-success fs-5">
+                    <i class="mdi mdi-door-open"></i>
+                    Re-Open
+                  </button>
+                  <button class="btn btn-danger fs-5">
+                    <i class="mdi mdi-delete-forever"></i>
+                    Delete
+                  </button>
                 </div>
-              </div>
-              <div class="d-flex gap-5">
-                <button class="btn btn-success fs-5">
-                  <i class="mdi mdi-door-open"></i>
-                  Re-Open
-                </button>
-                <button class="btn btn-danger fs-5">
-                  <i class="mdi mdi-delete-forever"></i>
-                  Delete
-                </button>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
