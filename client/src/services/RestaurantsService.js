e5d0437a66c7699723e246e131fb6ea44dff1ca0
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-23 09:27:46 -0700

    storing restaurants in appstate

--- client/src/services/RestaurantsService.js
+++ client/src/services/RestaurantsService.js
@@ -1,10 +1,13 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { AppState } from "@/AppState.js"
+import { Restaurant } from "@/models/Restaurant.js"
 
 class RestaurantsService {
   async getRestaurants() {
     const response = await api.get('api/restaurants')
     logger.log('GOT RESTAURANTS 🍽️🍽️🍽️', response.data)
+    AppState.restaurants = response.data.map(pojo => new Restaurant(pojo))
   }
 }
 
