0a7a1ba55912b9f6865925e2950699312e84bf0d
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-23 09:43:16 -0700

    styled up card

--- client/src/components/RestaurantCard.vue
+++ client/src/components/RestaurantCard.vue
@@ -8,8 +8,16 @@ defineProps({
 
 
 <template>
-  <div>
+  <div class="restaurant-card m-2">
     <img :src="restaurant.imgUrl" :alt="'A picture of ' + restaurant.name">
+    <div class="p-3">
+      <b class="text-success">{{ restaurant.name }}</b>
+      <p>{{ restaurant.description }}</p>
+      <div class="d-flex align-items-center">
+        <i class="mdi mdi-account-multiple-outline text-success fs-2"></i>
+        <span><b>{{ restaurant.visits }}</b> recent visits</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,5 +28,9 @@ img {
   height: 45dvh;
   object-fit: cover;
   object-position: center;
+}
+
+.restaurant-card {
+  box-shadow: 0 3px 10px rgb(81, 81, 81);
 }
 </style>
